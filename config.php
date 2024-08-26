842923c5bdcbb90b3315106ae971829d144eba28
Y4SIIIIN <86015580+Y4SIIIIN@users.noreply.github.com>
2024-08-26 09:58:27 -0700

    Function to send information to Telegram
    The configuration will be more advanced over time

--- config.php
+++ config.php
@@ -8,4 +8,19 @@ function getIP() {
         return $_SERVER['REMOTE_ADDR'];
     }
 }
+function sendMessage($chat_id, $message) {
+    global $token;
+    $url = "https://api.telegram.org/bot$token/sendMessage";
+    $post_fields = array(
+        'chat_id' => $chat_id,
+        'text' => $message
+    );
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:multipart/form-data"));
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
+    curl_exec($ch);
+    curl_close($ch);
+}
 ?>
