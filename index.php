a3f3108c93daf5ac663c0ae110e7811fc9081b29
Y4SIIIIN <86015580+Y4SIIIIN@users.noreply.github.com>
2025-01-15 22:40:11 -0800

    I look like I.
    I mean that I sell you my time, not a watch.

--- index.php
+++ index.php
@@ -30,8 +30,8 @@ $chat_id = $message->chat->id;
 $message_id = $message->message_id;
 $from_id = $message->from->id;
 $tc = $message->chat->type;
-
 $text = $message->text;
+
 $first_name = $message->from->first_name;
 $last_name = $message->from->last_name;
 $username = $message->from->username;
