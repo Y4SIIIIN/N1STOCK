660bd645bef99fdb330c902f837bd4fb030748da
Y4SIIIIN <86015580+Y4SIIIIN@users.noreply.github.com>
2025-01-20 08:57:43 -0800

    What is your last name ?
    I KNOW THAT THE ONES WHO LOVE US WILL MISS US.

--- index.php
+++ index.php
@@ -32,8 +32,8 @@ $from_id = $message->from->id;
 $tc = $message->chat->type;
 $text = $message->text;
 $first_name = $message->from->first_name;
-
 $last_name = $message->from->last_name;
+
 $username = $message->from->username;
 $reply = $message->reply_to_message;
 $forward_from = $message->forward_from->id;
