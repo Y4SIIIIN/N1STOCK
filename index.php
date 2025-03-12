0bf3802508bf1a2bd39b371b7531b4aebc9d9eab
Y4SIIIIN <86015580+Y4SIIIIN@users.noreply.github.com>
2025-03-11 18:54:37 -0700

    Charlie Prince
    Those who cling to death,live. 
    Those who cling to life,die.

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ $startKey = json_encode([
 			['text' => "PAY"], ['text' => "Subscriptions"] 
 		],
 		[
-			['text' => "Products"], ['text' => "Sellers Panel"]
+			['text' => "Products"], ['text' => "Sellers Panel"], ['text' => "Files"]
 		],
 			],     
 		"resize_keyboard" => true,	"one_time_keyboard" => false,	"input_field_placeholder" => "Please click ↓" 
