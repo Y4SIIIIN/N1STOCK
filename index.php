f7f25900aa4f1bd560802cec9ecc917fb95e8e9b
Y4SIIIIN <86015580+Y4SIIIIN@users.noreply.github.com>
2025-03-13 01:57:44 -0700

    Snoopy
    Even if you are the CEO of NVIDIA, a 17-year-old kid will call you a Bot or ChatGPT and ask you to buy their ticket and T-shirt.
    
    I think the best way is to sign up for a football club or other sports.
    The important thing is to eat at Nusr-Et restaurants and become an actor or singer.
    Bots can like and comment and buy subscriptions.
    Other jobs are mainly Bots or dogs.

--- index.php
+++ index.php
@@ -61,6 +61,9 @@ $startKey = json_encode([
 		[
 			['text' => "Products"], ['text' => "Sellers Panel"], ['text' => "Files"]
 		],
+	        [
+           		['text' => "Invite People"]
+        	],
 			],     
 		"resize_keyboard" => true,	"one_time_keyboard" => false,	"input_field_placeholder" => "Please click ↓" 
 		]);
