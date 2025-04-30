bc449d4ca101dea6ab4e01f46088330025e657e3
Y4SIIIIN <86015580+Y4SIIIIN@users.noreply.github.com>
2025-04-30 03:01:55 -0700

    Gravity
    What are you without storage, battery, processor, and network connections ?
    Someone trying to move fast enough to enter Earth’s orbit.

--- config.php
+++ config.php
@@ -1,13 +1,21 @@
 <?php
-function getIP() {
-    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-        return $_SERVER['HTTP_CLIENT_IP'];
-    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-        return $_SERVER['HTTP_X_FORWARDED_FOR'];
-    } else {
-        return $_SERVER['REMOTE_ADDR'];
+//Time Will Tell How Much I Love You
+function getIP($ip = null, $deep_detect = true){
+    if(filter_var($ip, FILTER_VALIDATE_IP) === false) {
+        $ip = $_SERVER["REMOTE_ADDR"];
+        if($deep_detect) {
+            if(filter_var(@$_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP))
+                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            if(filter_var(@$_SERVER['HTTP_CLIENT_IP'], FILTER_VALIDATE_IP))
+                $ip = $_SERVER['HTTP_CLIENT_IP'];
+        }
     }
+	else {
+        $ip = $_SERVER["REMOTE_ADDR"];
+    }
+    return $ip;
 }
+
 function sendMessage($chat_id, $message) {
     global $token;
     $url = "https://api.telegram.org/bot$token/sendMessage";
