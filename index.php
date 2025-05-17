90e0f716cba074ebea18e8bda3ab500d3b2f08bf
Y4SIIIIN <86015580+Y4SIIIIN@users.noreply.github.com>
2025-05-17 08:33:37 -0700

    Heil Hitler , No Name. No Other Alias
    Welcome to the Colosseum Games.

--- index.php
+++ index.php
@@ -41,6 +41,7 @@ $from_id = $message->from->id;
 $tc = $message->chat->type;
 # Creativity may begin alone, but impact needs infrastructure.
 $text = $message->text;
+# Hi Slave , What's Your Name ?
 $first_name = $message->from->first_name;
 $last_name = $message->from->last_name;
 $username = $message->from->username;
