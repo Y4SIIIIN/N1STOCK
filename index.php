0f598a063cf680970faa3ea044a18c64400b6bcf
Y4SIIIIN <86015580+Y4SIIIIN@users.noreply.github.com>
2025-09-30 05:37:25 -0700

    Thanks
    This is your last chance. After this, there’s no turning back.
    If you choose the pink high heels, your perfect, carefree life in Barbie Land continues as it is.
    But if you put on the simple flat shoes… you enter the real world,
    and you’ll find out just how deep the rabbit hole goes.
    Check out Y4SIIIIN's video! #TikTok https://vm.tiktok.com/ZNdG5HwxT/

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ $text = $message->text;
 $first_name = $message->from->first_name;
 # Elle est pas jolie, elle est pas moche non plus
 $last_name = $message->from->last_name;
-# When my last name becomes my brand, I will be the one who remains.
+# PLEASE COPY THIS SO YOU CAN FIND YOURSELF
 $username = $message->from->username;
 $reply = $message->reply_to_message;
 # Nod If You Understand
